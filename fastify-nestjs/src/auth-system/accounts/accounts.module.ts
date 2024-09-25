fa83342487fb5b13f022d26dee17fbf563828ece
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-26 00:46:34 +0545

    remove account controller

--- fastify-nestjs/src/auth-system/accounts/accounts.module.ts
+++ fastify-nestjs/src/auth-system/accounts/accounts.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
-import { AccountsController } from './accounts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
 
@@ -8,7 +7,6 @@ import { Account } from './entities/account.entity';
   imports: [
     TypeOrmModule.forFeature([Account]),
   ],
-  controllers: [AccountsController],
   providers: [AccountsService],
   exports: [AccountsService],
 })
