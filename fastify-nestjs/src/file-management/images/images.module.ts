d40ae7a09569708e835a8858530834f10022965a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-26 00:20:43 +0545

    fix: module path

--- fastify-nestjs/src/file-management/images/images.module.ts
+++ fastify-nestjs/src/file-management/images/images.module.ts
@@ -3,7 +3,7 @@ import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './entities/image.entity';
-import { AccountsModule } from 'src/accounts/accounts.module';
+import { AccountsModule } from 'src/auth-system/accounts/accounts.module';
 
 Global()
 @Module({
