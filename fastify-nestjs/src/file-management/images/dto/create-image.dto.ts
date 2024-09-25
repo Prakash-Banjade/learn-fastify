00fb97e2e640fff70ecf5e9951a92668a344ce74
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-26 00:56:58 +0545

    fix api documentation

--- fastify-nestjs/src/file-management/images/dto/create-image.dto.ts
+++ fastify-nestjs/src/file-management/images/dto/create-image.dto.ts
@@ -1,16 +1,16 @@
-import { ApiProperty, OmitType } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { HasMimeType, IsFile, MaxFileSize, MemoryStoredFile } from "nestjs-form-data";
 
 export class CreateImageDto {
-    @ApiProperty({ type: [String], format: 'binary', description: 'Image' })
+    @ApiProperty({ type: String, format: 'binary', description: 'Image file', isArray: true })
     @HasMimeType(['image/png', 'image/jpg', 'image/jpeg', 'image/webp'], { each: true })
     @IsFile({ each: true })
-    @MaxFileSize(5 * 1024 * 1024, { each: true })
+    @MaxFileSize(5 * 1024 * 1024, { each: true, message: 'Image size should be less than 5MB' })
     @IsNotEmpty({ each: true })
     images: MemoryStoredFile[]
 
-    @ApiProperty()
+    @ApiPropertyOptional({ type: String, description: 'Image name' })
     @IsString()
     @IsOptional()
     name?: string
