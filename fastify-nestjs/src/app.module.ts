1e06142d8334fba58ae0cc5f3405c58ea4a8c502
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-26 00:20:33 +0545

    inject form data module

--- fastify-nestjs/src/app.module.ts
+++ fastify-nestjs/src/app.module.ts
@@ -5,11 +5,23 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from './datasource/typeorm.module';
 import { AuthSystemModule } from './auth-system/auth-system.module';
 import { FileManagementModule } from './file-management/file-management.module';
+import { MemoryStoredFile, NestjsFormDataModule } from 'nestjs-form-data';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    NestjsFormDataModule.config({
+      storage: MemoryStoredFile,
+      isGlobal: true,
+      fileSystemStoragePath: 'public',
+      autoDeleteFile: false,
+      limits: {
+        files: 10,
+        fileSize: 5 * 1024 * 1024,
+      },
+      cleanupAfterSuccessHandle: false, // !important
     }),
     TypeOrmModule,
     AuthSystemModule,
