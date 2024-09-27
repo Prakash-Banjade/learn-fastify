1896406b04cfd45f756de4c329c4920c8fd41ddc
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-27 16:40:54 +0545

    add global validation pipe

--- fastify-nestjs/src/main.ts
+++ fastify-nestjs/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AllExceptionsFilter } from './common/filters/all-exception.filter';
 import { setupSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +14,13 @@ async function bootstrap() {
   // global exception filter
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // global validation pipe
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true, 
+  }));
   
   // swagger docs setup
   setupSwagger(app);
