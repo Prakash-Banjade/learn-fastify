1a26316eadfd92a4741816edbf40f1cdb248a3e2
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-05 14:24:53 +0545

    remove authentication functionality with JWT strategy

--- fastify-nestjs/src/auth-system/auth-system.module.ts
+++ fastify-nestjs/src/auth-system/auth-system.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { CaslModule } from './casl/casl.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AbilitiesGuard } from 'src/common/guards/abilities.guard';
-import { AuthGuard } from 'src/common/guards/auth.guard';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -15,14 +12,7 @@ import { AuthModule } from './auth/auth.module';
         AuthModule,
     ],
     providers: [
-        {
-            provide: APP_GUARD,
-            useClass: AuthGuard, // global auth guard
-        },
-        {
-            provide: APP_GUARD,
-            useClass: AbilitiesGuard, // global ability guard
-        },
+      
     ]
 })
 export class AuthSystemModule { }
--- fastify-nestjs/src/auth-system/auth/auth.module.ts
+++ fastify-nestjs/src/auth-system/auth/auth.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { ImagesModule } from 'src/file-management/images/images.module';
 import { AccountsModule } from '../accounts/accounts.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.ACCESS_TOKEN_SECRET!,
-      signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION! },
-    }),
     AccountsModule,
     UsersModule,
     ImagesModule,
