b05d9bf5c61277bb1d1894d4c3994618d1309003
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-06 23:51:31 +0545

    add regex pattern for name

--- fastify-nestjs/src/auth-system/auth/dto/register.dto.ts
+++ fastify-nestjs/src/auth-system/auth/dto/register.dto.ts
@@ -1,17 +1,24 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword, Length } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword, Length, Matches } from "class-validator";
+import { NAME_REGEX } from "src/common/CONSTANTS";
 
 export class RegisterDto {
     @ApiProperty({ type: 'string', description: 'First name of the user' })
     @IsString()
     @IsNotEmpty()
     @Length(2)
+    @Matches(NAME_REGEX, {
+        message: 'Name must not have special characters'
+    })
     firstName!: string;
 
     @ApiProperty({ type: 'string', description: 'Last name of the user' })
     @IsString()
     @Length(2)
     @IsOptional()
+    @Matches(NAME_REGEX, {
+        message: 'Name must not have special characters'
+    })
     lastName?: string;
 
     @ApiProperty({ type: 'string', format: 'email', description: 'Valid email' })
