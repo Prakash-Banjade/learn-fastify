c9ee6621031e7c8dc6ea2c9a5aed5e301adb4df4
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-08 19:00:02 +0545

    separate jwt module, update verify-email service

--- fastify-nestjs/src/auth-system/auth/auth.service.ts
+++ fastify-nestjs/src/auth-system/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import {
   Inject,
   Injectable,
+  NotFoundException,
   Scope,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { PasswordChangeRequest } from './entities/password-change-request.entity';
@@ -12,15 +12,15 @@ import { REQUEST } from '@nestjs/core';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { Account } from '../accounts/entities/account.entity';
 import { User } from '../users/entities/user.entity';
-import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { AuthUser } from 'src/common/types/global.type';
-import { CookieSerializeOptions } from '@fastify/cookie';
 import { Tokens } from 'src/common/CONSTANTS';
 import { RegisterDto } from './dto/register.dto';
 import { SignInDto } from './dto/signIn.dto';
 import { MailService } from 'src/mail/mail.service';
 import { AuthHelper } from './helpers/auth.helper';
+import { JwtService } from '../jwt/jwt.service';
+import { EmailVerificationDto } from './dto/email-verification.dto';
 
 @Injectable({ scope: Scope.REQUEST })
 export class AuthService extends BaseRepository {
@@ -37,10 +37,6 @@ export class AuthService extends BaseRepository {
   private readonly usersRepo = this.datasource.getRepository<User>(User)
   private readonly emailVerificationPendingRepo = this.datasource.getRepository<EmailVerificationPending>(EmailVerificationPending)
   private readonly passwordChangeRequestRepo = this.datasource.getRepository<PasswordChangeRequest>(PasswordChangeRequest);
-  private readonly ACCESS_TOKEN_SECRET = this.configService.getOrThrow<string>('ACCESS_TOKEN_SECRET');
-  private readonly ACCESS_TOKEN_EXPIRATION_MS = this.configService.getOrThrow<string>('ACCESS_TOKEN_EXPIRATION_MS');
-  private readonly REFRESH_TOKEN_SECRET = this.configService.getOrThrow<string>('REFRESH_TOKEN_SECRET');
-  private readonly REFRESH_TOKEN_EXPIRATION_MS = this.configService.getOrThrow<string>('REFRESH_TOKEN_EXPIRATION_MS');
 
   async login(signInDto: SignInDto, req: FastifyRequest, reply: FastifyReply) {
     const existingRefreshToken = req.cookies?.refresh_token;
@@ -51,51 +47,48 @@ export class AuthService extends BaseRepository {
     const payload: AuthUser = {
       email: foundAccount.email,
       accountId: foundAccount.id,
-      userId: foundAccount.user.id,
       role: foundAccount.role,
     };
 
-    const access_token = await this.createAccessToken(payload);
-    const refresh_token = await this.createRefreshToken(payload);
+    const { access_token, refresh_token } = await this.jwtService.getAuthTokens(payload);
 
     const newRefreshTokenArray = !refresh_token ? (foundAccount.refreshTokens ?? []) : (foundAccount?.refreshTokens?.filter((rt) => rt !== existingRefreshToken) ?? [])
-    if (refresh_token) reply.clearCookie(Tokens.REFRESH_TOKEN_COOKIE_NAME, this.getCookieOptions(Tokens.REFRESH_TOKEN_COOKIE_NAME)); // CLEAR COOKIE, BCZ A NEW ONE IS TO BE GENERATED
+    if (refresh_token) reply.clearCookie(Tokens.REFRESH_TOKEN_COOKIE_NAME, this.jwtService.getCookieOptions(Tokens.REFRESH_TOKEN_COOKIE_NAME)); // CLEAR COOKIE, BCZ A NEW ONE IS TO BE GENERATED
 
     foundAccount.refreshTokens = [...newRefreshTokenArray, refresh_token];
 
-    reply.setCookie(Tokens.REFRESH_TOKEN_COOKIE_NAME, refresh_token, this.getCookieOptions(Tokens.REFRESH_TOKEN_COOKIE_NAME))
+    reply.setCookie(Tokens.REFRESH_TOKEN_COOKIE_NAME, refresh_token, this.jwtService.getCookieOptions(Tokens.REFRESH_TOKEN_COOKIE_NAME))
 
     await this.accountsRepo.save(foundAccount);
 
     return { access_token };
   }
 
-  async createAccessToken(payload: AuthUser): Promise<string> {
-    return await this.jwtService.signAsync(payload, {
-      secret: this.ACCESS_TOKEN_SECRET,
-      expiresIn: this.ACCESS_TOKEN_EXPIRATION_MS,
+  async verifyEmail(emailVerificationDto: EmailVerificationDto) {
+    const foundRequest = await this.authHelper.verifyEmail(emailVerificationDto);
+
+    // GET ACCOUNT FROM DATABASE
+    const foundAccount = await this.accountsRepo.findOneBy({ email: foundRequest.email });
+    if (!foundAccount) throw new NotFoundException('Account not found');
+
+    foundAccount.isVerified = true;
+    const savedAccount = await this.accountsRepo.save(foundAccount);
+
+    const newUser = this.usersRepo.create({
+      account: savedAccount,
     });
-  }
 
-  async createRefreshToken(payload: Pick<AuthUser, 'accountId'>): Promise<string> {
-    return await this.jwtService.signAsync(
-      { accountId: payload.accountId },
-      {
-        secret: this.REFRESH_TOKEN_SECRET,
-        expiresIn: this.REFRESH_TOKEN_EXPIRATION_MS,
+    await this.usersRepo.save(newUser);
+
+    await this.emailVerificationPendingRepo.remove(foundRequest); // remove from db
+
+    return {
+      message: 'Account verified successfully',
+      account: {
+        email: savedAccount.email,
+        name: savedAccount.firstName + ' ' + savedAccount.lastName,
       },
-    );
-  }
-
-  private getCookieOptions(tokenType: Tokens.ACCESS_TOKEN_COOKIE_NAME | Tokens.REFRESH_TOKEN_COOKIE_NAME): CookieSerializeOptions {
-    const cookieOptions: CookieSerializeOptions = {
-      httpOnly: true,
-      secure: this.configService.getOrThrow('NODE_ENV') === 'production',
-      sameSite: 'strict',
-      maxAge: tokenType === Tokens.ACCESS_TOKEN_COOKIE_NAME ? Number(this.ACCESS_TOKEN_EXPIRATION_MS) : Number(this.REFRESH_TOKEN_EXPIRATION_MS),
     };
-
-    return cookieOptions;
   }
 
   async register(registerDto: RegisterDto) {
